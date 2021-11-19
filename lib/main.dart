c9927c46ca8ca4812a8ef6d8f04c8483fb173b38
Vitor Fernandes Dallegrave <vitordallegrave@gmail.com>
2021-11-18 22:55:58 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(MyApp());
+
+//  Firestore.instance.collection('coll').document('doc').setData({'texto': "Daniel"});
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}

88fc715d88e3a16e11d9817f351a510fba6dd725
Vitor Fernandes Dallegrave <vitordallegrave@gmail.com>
2021-11-23 23:17:41 -0300

    Configuração da FireStore, update do projeto completo.

---
+++ android/app/src/main/kotlin/pibid/dallegrave/app/recife_go/MainActivity.kt
@@ -0,0 +1,6 @@
+package pibid.dallegrave.app.recife_go
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:recife_go/Telas/Home_Page.dart';
 
-void main() {
 
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
 
-//  Firestore.instance.collection('coll').document('doc').setData({'texto': "Daniel"});
-
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
-}
+  Firestore.instance.collection('col').document('doc').setData("{"texto": "daniel"}");
+  title: "RecifeGo!",
+  home: Home_Page(),
+// remover a tag de bug no canto superior debugShowCheckedModeBanner: false,
+));
